fe5650fdfa5835d01e69d81767f80fdd7919595f
Sila678 <silanaris24@gmail.com>
2025-11-24 00:03:03 -0500

    JS completed

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.querySelector('.newsletter form');
+    const email_Input = document.querySelector('#email');
+    const message_Div = document.querySelector ('.message');
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault(); //Prevent page reload.
+
+        const email = email_Input.value.trim();
+
+        if (email === "") {
+            message_Div.textContent = "Please enter a valid email address.";
+            message_Div.style.color = "white";
+        } else {
+            message_Div.textContent = `Thank you! Your email address ${email} has been added to our mailing list.`;
+            message_Div.style.color = "white";
+
+            email_Input.value = ""; //Clear the input.
+        }
+    });
+});
